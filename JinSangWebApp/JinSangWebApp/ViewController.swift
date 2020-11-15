03f4812b46f8ced60ba38fdbeb1cc9196bdf4d15
sanghun0724 <sanghun0724@naver.com>
2020-11-15 21:27:17 +0900

    JinSangWebApp

---
+++ JinSangWebApp/JinSangWebApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JinSangWebApp
+//
+//  Created by sangheon on 2020/11/15.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
